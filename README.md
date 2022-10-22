420853c95b6b202a7b2f3732b5ed45cc6382f8c0
Ponthea Zahraii <pontheazahraii@gmail.com>
2022-10-21 18:51:46 -0700

    hover over buttons

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 Sources
 https://www.geeksforgeeks.org/how-to-draw-rectangle-in-pygame/
+https://www.geeksforgeeks.org/how-to-create-buttons-in-a-game-using-pygame/
--- window.py
+++ window.py
@@ -21,23 +21,40 @@ while True:
             pygame.quit()
             exit()
 
+    mouse = pygame.mouse.get_pos()
+
     screen.blit(background, (0,0))
     screen.blit(graph_background, (275, 20))
-    pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 20, 150, 75), 100, 3)
+
+    
+    if 10 <= mouse[0] <= 160 and 20 <= mouse[1] <= 95:
+        pygame.draw.rect(screen, 'Red', pygame.Rect(10, 20, 150, 75), 100, 3)
+    else:
+        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 20, 150, 75), 100, 3)
     pygame.draw.rect(screen, 'Black', pygame.Rect(10, 20, 150, 75), 2, 3)
 
-    pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 100, 150, 75), 100, 3)
+    if 10 <= mouse[0] <= 160 and 100 <= mouse[1] <= 175:
+        pygame.draw.rect(screen, 'Orange', pygame.Rect(10, 100, 150, 75), 100, 3)
+    else:
+        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 100, 150, 75), 100, 3)
     pygame.draw.rect(screen, 'Black', pygame.Rect(10, 100, 150, 75), 2, 3)
-    
-    pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 180, 150, 75), 100, 3)
+
+    if 10 <= mouse[0] <= 160 and 180 <= mouse[1] <= 255:
+        pygame.draw.rect(screen, 'Yellow', pygame.Rect(10, 180, 150, 75), 100, 3)
+    else:
+        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 180, 150, 75), 100, 3)
     pygame.draw.rect(screen, 'Black', pygame.Rect(10, 180, 150, 75), 2, 3)
 
-    pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 260, 150, 75), 100, 3)
+    if 10 <= mouse[0] <= 160 and 260 <= mouse[1] <= 335:
+        pygame.draw.rect(screen, 'Green', pygame.Rect(10, 260, 150, 75), 100, 3)
+    else:
+        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 260, 150, 75), 100, 3)
     pygame.draw.rect(screen, 'Black', pygame.Rect(10, 260, 150, 75), 2, 3)
 
-    pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 340, 150, 75), 100, 3)
+    if 10 <= mouse[0] <= 160 and 340 <= mouse[1] <= 415:
+        pygame.draw.rect(screen, 'Blue', pygame.Rect(10, 340, 150, 75), 100, 3)
+    else:
+        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 340, 150, 75), 100, 3)
     pygame.draw.rect(screen, 'Black', pygame.Rect(10, 340, 150, 75), 2, 3)
-
-    pygame.display.flip()
 
     pygame.display.update()
