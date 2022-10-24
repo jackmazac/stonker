c21b23b66845107a2076f1dcdc75f319978ce9bc
Ponthea Zahraii <pontheazahraii@gmail.com>
2022-10-23 21:05:27 -0700

    made a button method

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 Sources
 https://www.geeksforgeeks.org/how-to-draw-rectangle-in-pygame/
 https://www.geeksforgeeks.org/how-to-create-buttons-in-a-game-using-pygame/
+https://stackoverflow.com/questions/48093361/using-matplotlib-in-pygame
--- window.py
+++ window.py
@@ -1,6 +1,31 @@
 from turtle import back
 import pygame
 from sys import exit 
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.backends.backend_agg as agg
+import pylab
+
+def hover_button(screen, color, a):
+    if 10 <= mouse[0] <= 160 and a <= mouse[1] <= a+75:
+        pygame.draw.rect(screen, color, pygame.Rect(10, a, 150, 75), 100, 3)
+    else:
+        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, a, 150, 75), 100, 3)
+    pygame.draw.rect(screen, 'Black', pygame.Rect(10, a, 150, 75), 2, 3)
+
+def button(a):
+    pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, a, 150, 75), 100, 3)
+    pygame.draw.rect(screen, 'Black', pygame.Rect(10, a, 150, 75), 2, 3)
+
+fig = pylab.figure(figsize=[7,3.95], dpi = 100) 
+
+ax = fig.gca()
+ax.plot([1,2,4])
+
+canvas = agg.FigureCanvasAgg(fig)
+canvas.draw()
+renderer = canvas.get_renderer()
+raw_data = renderer.tostring_rgb()
 
 pygame.init()
 
@@ -14,47 +39,28 @@ background.fill('Light Grey')
 
 graph_background = pygame.Surface((700,395))
 graph_background.fill('Black')
+graph = pygame.image.fromstring(raw_data, (700,395), "RGB")
+
+smallfont = pygame.font.SysFont('Corbel', 35)
+text = smallfont.render('clicked', True, 'Black')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+            
     mouse = pygame.mouse.get_pos()
 
     screen.blit(background, (0,0))
     screen.blit(graph_background, (275, 20))
+    screen.blit(graph,(275, 20))
+    pygame.draw.rect(screen, 'Black', pygame.Rect(275, 20, 700, 395), 2, 3)
 
-    
-    if 10 <= mouse[0] <= 160 and 20 <= mouse[1] <= 95:
-        pygame.draw.rect(screen, 'Red', pygame.Rect(10, 20, 150, 75), 100, 3)
-    else:
-        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 20, 150, 75), 100, 3)
-    pygame.draw.rect(screen, 'Black', pygame.Rect(10, 20, 150, 75), 2, 3)
-
-    if 10 <= mouse[0] <= 160 and 100 <= mouse[1] <= 175:
-        pygame.draw.rect(screen, 'Orange', pygame.Rect(10, 100, 150, 75), 100, 3)
-    else:
-        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 100, 150, 75), 100, 3)
-    pygame.draw.rect(screen, 'Black', pygame.Rect(10, 100, 150, 75), 2, 3)
-
-    if 10 <= mouse[0] <= 160 and 180 <= mouse[1] <= 255:
-        pygame.draw.rect(screen, 'Yellow', pygame.Rect(10, 180, 150, 75), 100, 3)
-    else:
-        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 180, 150, 75), 100, 3)
-    pygame.draw.rect(screen, 'Black', pygame.Rect(10, 180, 150, 75), 2, 3)
-
-    if 10 <= mouse[0] <= 160 and 260 <= mouse[1] <= 335:
-        pygame.draw.rect(screen, 'Green', pygame.Rect(10, 260, 150, 75), 100, 3)
-    else:
-        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 260, 150, 75), 100, 3)
-    pygame.draw.rect(screen, 'Black', pygame.Rect(10, 260, 150, 75), 2, 3)
-
-    if 10 <= mouse[0] <= 160 and 340 <= mouse[1] <= 415:
-        pygame.draw.rect(screen, 'Blue', pygame.Rect(10, 340, 150, 75), 100, 3)
-    else:
-        pygame.draw.rect(screen, 'Dark Grey', pygame.Rect(10, 340, 150, 75), 100, 3)
-    pygame.draw.rect(screen, 'Black', pygame.Rect(10, 340, 150, 75), 2, 3)
+    hover_button(screen, 'Red', 20)
+    hover_button(screen, 'Orange', 100)
+    hover_button(screen, 'Yellow', 180)
+    hover_button(screen, 'Green', 260)
+    hover_button(screen, 'Blue', 340)
 
     pygame.display.update()
